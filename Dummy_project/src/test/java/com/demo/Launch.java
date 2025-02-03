952db59d135c328255c22f400a0ffadb6ba1b2e1
Rajlaxmi <rajlaxmia23@gmail.com>
2025-02-03 08:16:19 +0530

    commit

---
+++ Dummy_project/src/test/java/com/demo/Launch.java
@@ -0,0 +1,21 @@
+package com.demo;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Launch {
+@Test
+public void toLaunch() {
+	WebDriver driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+	
+	driver.get("https://demoapp.skillrary.com/");
+	Reporter.log("WebPage successfully launched", true);
+	driver.quit();
+}
+}
