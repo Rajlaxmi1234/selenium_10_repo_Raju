08ebf47d9762dbc099a5bcea73ed949a1ca89597
dell <dell@DESKTOP-M1U6SU2>
2025-02-04 11:26:31 +0530

    aman sinha

---
+++ Dummy_project/src/test/java/Aman/pullAndFork.java
@@ -0,0 +1,19 @@
+package Aman;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class pullAndFork {
+
+
+		// TODO Auto-generated method stub
+@Test
+public void pull() {
+	Reporter.log("aman sinha",true);
+}
+
+	
+
+	}
+
+
